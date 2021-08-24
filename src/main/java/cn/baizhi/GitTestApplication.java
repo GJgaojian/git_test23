019c086142c79706f037c43d08af66bf4e89b792
gaojian <13141234762@163.com>
2021-08-24 12:52:07 +0800

    ff

---
+++ src/main/java/cn/baizhi/GitTestApplication.java
@@ -0,0 +1,16 @@
+package cn.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+    public static void main(String[] args) {
+
+        //souttttttt
+
+        SpringApplication.run(GitTestApplication.class, args);
+    }
+
+}
